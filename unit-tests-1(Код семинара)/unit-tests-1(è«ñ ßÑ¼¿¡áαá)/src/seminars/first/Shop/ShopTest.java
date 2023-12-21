75b79858a967d6e73ca916bce7f4804261233cbb
malgepav <malgepav@yandex.ru>
2023-12-21 15:39:05 +0300

    DZ1_task2

--- unit-tests-1(Код семинара)/unit-tests-1(è«ñ ßÑ¼¿¡áαá)/src/seminars/first/Shop/ShopTest.java
+++ unit-tests-1(Код семинара)/unit-tests-1(è«ñ ßÑ¼¿¡áαá)/src/seminars/first/Shop/ShopTest.java
@@ -1,5 +1,10 @@
 package seminars.first.Shop;
 
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class ShopTest {
 
     /*
@@ -7,5 +12,39 @@ public class ShopTest {
    2. Напишите тесты для проверки корректности работы метода getMostExpensiveProduct.
    3. Напишите тесты для проверки корректности работы метода sortProductsByPrice (проверьте правильность сортировки).
    */
+    public static void main(String[] args) {
+        Product apples  = new Product();
+        apples.setCost(100);;
+        apples.setTitle("Apples");
 
+        Product bread  = new Product();
+        bread.setCost(50);;
+        bread.setTitle("Bread");
+
+        Product caviar  = new Product();
+        caviar.setCost(5000);
+        caviar.setTitle("Caviar");
+
+        Product cheese  = new Product();
+        cheese.setTitle("Cheese");
+        cheese.setCost(300);
+
+        List<Product> productList = List.of(apples,bread,caviar,cheese);
+        Shop shop = new Shop();
+        shop.setProducts(productList);
+
+
+        //1. Напишите тесты, чтобы проверить, что магазин хранит верный список продуктов
+        // (правильное количество продуктов, верное содержимое корзины).
+
+        assertThat(shop.getProducts()).hasSize(productList.size()).containsAll(productList);
+
+        //2. Напишите тесты для проверки корректности работы метода getMostExpensiveProduct.
+
+        assertThat(shop.getMostExpensiveProduct().getTitle())
+                .isEqualTo("Caviar");
+        //3. Напишите тесты для проверки корректности работы метода
+        // sortProductsByPrice (проверьте правильность сортировки).
+        assertThat(shop.sortProductsByPrice()).isSortedAccordingTo(Comparator.comparingInt(Product::getCost));
+    }
 }
